15233692674310d156f9beb66f50aa8dddef8c5c
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2025-03-23 15:37:32 +0100

    Add slurm scripts

--- slurm/submit_jobs.sh
+++ slurm/submit_jobs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-learning_rates=(0.0001 0.0005 0.001 0.005 0.01 0.05)
-weight_decays=(0.0 0.1)
+learning_rates=(0.00001 0.0001 0.001 0.01)
+weight_decays=(0.0)
 
 log_file="submitted_jobs.txt"
 echo "Submitting jobs at $(date)" > $log_file
--- slurm/train.sh
+++ slurm/train.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --time=3-00:00
 #SBATCH --gres=gpu:1
-#SBATCH --mem=100G
+#SBATCH --mem=150G
 #SBATCH --partition=a100-galvani
 #SBATCH --output=/mnt/lustre/work/bethge/dziadzio08/projects/specops/slurm/specops_%j.out
 #SBATCH --error=/mnt/lustre/work/bethge/dziadzio08/projects/specops/slurm/specops_%j.err
@@ -13,8 +13,9 @@
 
 scontrol show job $SLURM_JOB_ID
 
-source $HOME/.bashrc
 conda activate soups
+source $HOME/.bashrc
+export WORK='/mnt/lustre/work/bethge/dziadzio08'
 
 additional_args="$@"
 python $HOME/repos/soups/specops.py $additional_args
